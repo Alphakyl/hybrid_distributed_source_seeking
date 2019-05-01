44182f8541c221048302a398ed8a7f4337c12e76
Alphakyl <kyle.harlow@colorado.edu>
2019-04-30 19:42:33 -0600

    Fixed typo, and swapped rectified dimension k, and robot number n errors

--- distributed_control.m
+++ distributed_control.m
@@ -1,14 +1,14 @@
 function [u] = distributed_control(x,x_d,c_0,g_ce,v,z5,c_1,c_2, L_1, L_2)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
-k = size(x,2);
-n = size(x,1);
+k = size(x,1);
+n = size(x,2);
 
 for ii = 1:n
     temp1 = 0;
     temp3 = 0;
     for jj = 1:n
-        if (L1(ii,jj)~=0 && (ii~=jj))
+        if (L_1(ii,jj)~=0 && (ii~=jj))
             % sum_j\in F(i) w1ij(x_i-x_j-x_di+x_dj)
             temp1 = temp1+-L_1(ii,jj)*(x(:,ii)-x(:,jj)-x_d(:,ii)+x_d(:,jj));
             % sum_j\in F(i) w2ij(vi-vj)
@@ -16,7 +16,7 @@ for ii = 1:n
         end
     end
     % c_0 gc
-    temp2 = c_0*g_ce';
+    temp2 = c_0*g_ce(:,ii);
     % c_1/n*sum(vi)
     temp4 = c_1*z5(:,ii);
     % c2sign(sum(v_i))
