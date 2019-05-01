b31ef1193e41a8b43bc2fd33e81399774f3863e5
Alphakyl <kyle.harlow@colorado.edu>
2019-04-30 18:48:48 -0600

    Fixed function naming in RK4_Z1

--- RK4_Z1.m
+++ RK4_Z1.m
@@ -4,8 +4,8 @@ function [Z1] = RK4_Z1(x,Z1,gamma,adjacency)
     %k_1 = h*robot_position(v);
     k_1 = h*Z1_consensus(x,Z1,gamma,adjacency)
     k_2 = h*Z1_consensus(x,Z1+(1/2)*k_1,gamma,adjacency);
-    k_3 = h*robot_position(x,Z1+(1/2)*k_2,gamma,adjacency);
-    k_4 = h*robot_position(x,Z1+k_3*1,gamma,adjacency);
+    k_3 = h*Z1_consensus(x,Z1+(1/2)*k_2,gamma,adjacency);
+    k_4 = h*Z1_consensus(x,Z1+k_3*1,gamma,adjacency);
     Z1 = Z1+1/6*(k_1+2*k_2+2*k_3+k_4);
 end
 
