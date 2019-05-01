79b1e94245b179389fa4c394927e74904588501b
Alphakyl <kyle.harlow@colorado.edu>
2019-04-30 20:22:30 -0600

    Suppressed K1 on RK4 for z2-4

--- RK4_Z2.m
+++ RK4_Z2.m
@@ -2,7 +2,7 @@ function [Z2] = RK4_Z2(x,Z2,gamma,adjacency,h)
 %UNTITLED17 Summary of this function goes here
 %   Detailed explanation goes here
     %k_1 = h*robot_position(v);
-    k_1 = h*z2_consensus(x,Z2,gamma,adjacency)
+    k_1 = h*z2_consensus(x,Z2,gamma,adjacency);
     k_2 = h*z2_consensus(x,Z2+(1/2)*k_1,gamma,adjacency);
     k_3 = h*z2_consensus(x,Z2+(1/2)*k_2,gamma,adjacency);
     k_4 = h*z2_consensus(x,Z2+k_3*1,gamma,adjacency);
